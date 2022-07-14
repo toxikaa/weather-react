6a0157233f3e9bd4c47ceceacea317df047e8139
toxikaa <107145902+toxikaa@users.noreply.github.com>
2022-07-14 16:39:09 +0300

    Added react

--- src/Weather.js
+++ src/Weather.js
@@ -5,21 +5,47 @@ import axios from "axios";
 library.add(faMagnifyingGlass);
 
 export default function Weather() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("Paris");
   const [weather, setWeather] = useState("");
-
+  const [icon, setIcon] = useState("");
   function changedCity(event) {
     setCity(event.target.value);
   }
 
-  let cityName = "Paris";
-  let weatherDescription = "Sunny";
-  let minDegree = 35;
-  let maxDegree = " " + 40;
-  let temeratureValue = 22;
-  let fullTimeValue = "14 June, Tuesday 11:44";
-  let humididty = 84;
-  let wind = 10;
+  function showWeather(response) {
+    setWeather({
+      cityName: response.data.name,
+      weatherDescription: response.data.weather[0].main,
+      minDegree: Math.round(response.data.main.temp_min),
+      maxDegree: Math.round(response.data.main.temp_max),
+      temeratureValue: Math.round(response.data.main.temp),
+      fullTimeValue: response.data.dt,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+    });
+    setIcon(`"./${response.data.weather[0].main}.png"`);
+  }
+
+  function fahrenheit(event) {
+    event.preventDefault();
+    setWeather({
+      cityName: weather.cityName,
+      weatherDescription: weather.weatherDescription,
+      minDegree: Math.round((weather.minDegree * 9) / 5 + 32),
+      maxDegree: Math.round((weather.maxDegree * 9) / 5 + 32),
+      temeratureValue: Math.round((weather.temeratureValue * 9) / 5 + 32),
+      fullTimeValue: weather.fullTimeValue,
+      humidity: weather.humidity,
+      wind: weather.wind,
+    });
+  }
+
+  function sumbittedForm(event) {
+    event.preventDefault();
+    let myApiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${myApiKey}`;
+    axios.get(apiUrlCity).then(showWeather);
+  }
 
   return (
     <section className="Weather">
@@ -27,25 +53,26 @@ export default function Weather() {
         <div className="row">
           <div className="col-sm-6">
             <div className="glavnaya shadow-sm">
-              <h1 className="city">{cityName}</h1>
-              <h2 className="weather">{weatherDescription}</h2>
+              <h1 className="city">{weather.cityName}</h1>
+              <h2 className="weather">{weather.weatherDescription}</h2>
               <h5 className="min-max">
-                Min: <span className="min-temp">{minDegree}</span>º | Max:
-                <span className="max-temp">{maxDegree}</span>º
+                Min: <span className="min-temp">{weather.minDegree}</span>º |
+                Max:
+                <span className="max-temp"> {weather.maxDegree}</span>º
               </h5>
               <div className="row">
                 <div className="col-6">
-                  <p className="temp">{temeratureValue}º</p>
+                  <p className="temp">{weather.temeratureValue}º</p>
                 </div>
                 <div className="col-6">
                   <img
-                    src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/037/886/original/sunny.png?1655733776"
+                    src=""
                     width="130"
                     className="mainimage img-fluid"
                     alt=""
                   />
                 </div>
-                <p className="datatime">{fullTimeValue}</p>
+                <p className="datatime">1</p>
               </div>
               <div className="dopmeta">
                 <div className="row">
@@ -57,7 +84,7 @@ export default function Weather() {
                       <br />
                       <em>
                         <strong className="d-flex justify-content-center">
-                          {humididty}%
+                          {weather.humidity}%
                         </strong>
                       </em>
                     </p>
@@ -70,7 +97,7 @@ export default function Weather() {
                       <br />
                       <em>
                         <strong className="d-flex justify-content-center">
-                          {wind}km/h
+                          {weather.wind}km/h
                         </strong>
                       </em>
                     </p>
@@ -82,12 +109,12 @@ export default function Weather() {
           <div className="col-lg-6">
             <div className="d-flex justify-content-center currentLocationElement">
               <button className="loc">
-                <span class="material-symbols-outlined">near_me</span>
+                <span className="material-symbols-outlined">near_me</span>
               </button>
               <span className="currently">Current location</span>
             </div>
             <div className="formSearch d-flex justify-content-center">
-              <form className="search shadow">
+              <form className="search shadow" onSubmit={sumbittedForm}>
                 <input
                   type="text"
                   placeholder="Your city"
@@ -99,8 +126,8 @@ export default function Weather() {
             </div>
             <div className="degreeBnt d-flex justify-content-center">
               <span className="mx-3 ">
-                <a href="/">
-                  <span class="material-symbols-outlined turnoffbtn">
+                <a href="/" onClick={sumbittedForm}>
+                  <span className="material-symbols-outlined turnoffbtn">
                     power_settings_new
                   </span>
                 </a>
@@ -109,8 +136,8 @@ export default function Weather() {
                 </span>
               </span>
               <span>
-                <a href="/">
-                  <span class="material-symbols-outlined turnoffbtn">
+                <a href="/" onClick={fahrenheit}>
+                  <span className="material-symbols-outlined turnoffbtn">
                     power_settings_new
                   </span>
                 </a>
