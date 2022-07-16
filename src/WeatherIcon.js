c7ea42b87c29cf2d32f06ae28a9ba879ed35a780
toxikaa <107145902+toxikaa@users.noreply.github.com>
2022-07-16 22:27:10 +0300

    fix

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -4,7 +4,7 @@ export default function WeatherIcon(props) {
   let images = require(`./images/${props.code}.png`);
   return (
     <img
-      src={images}
+      src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/037/889/original/cloudy.png?1655733793"
       width="130"
       className="mainimage img-fluid"
       alt={props.code}
